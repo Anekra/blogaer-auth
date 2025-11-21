9ecb5200d810d7d47f1d41ada15636e3f943a0bc
anekra <andika.eka.102@gmail.com>
2025-11-21 23:20:33 +0700

    Improve: adding csrf token check.

--- src/encore/auths/auth/auth.ts
+++ src/encore/auths/auth/auth.ts
@@ -1,40 +1,49 @@
-import { Gateway } from 'encore.dev/api';
+import { APIError, Gateway } from 'encore.dev/api';
 import { authHandler } from 'encore.dev/auth';
-import mainModel from '../../../models/main-model';
-import { Op } from 'sequelize';
 import jwt from 'jsonwebtoken';
-import { AuthReq } from '../../../types/request';
-import { AccessDecoded, AuthData } from '../../../types';
+import { Op } from 'sequelize';
+import mainModel from '../../../models/main-model';
+import type { AccessDecoded, AuthData } from '../../../types';
+import type { AuthReq } from '../../../types/request';
 
-async function mainAuth({ authorization }: AuthReq): Promise<AuthData | null> {
-  if (!authorization.startsWith('Bearer')) return null;
-  const clientId = authorization.split(' ')[1];
-  if (!clientId || clientId === 'undefined' || clientId === 'null') return null;
+async function mainAuth({ authorization, csrf }: AuthReq): Promise<AuthData> {
+	if (!authorization.startsWith('Bearer')) {
+		throw APIError.unauthenticated('Bearer missing!');
+	}
+	if (!csrf || csrf === 'undefined' || csrf === 'null') {
+		throw APIError.unauthenticated('CSRF token missing!');
+	}
+	const clientId = authorization.split(' ')[1];
+	if (!clientId || clientId === 'undefined' || clientId === 'null') {
+		throw APIError.unauthenticated('Client ID missing!');
+	}
 
-  const model = await mainModel;
-  const token = await model.token.findOne({
-    where: {
-      clientId,
-      access: {
-        [Op.gte]: new Date(Date.now() - 15 * 60 * 1000)
-      }
-    }
-  });
-  if (!token) return null;
+	const model = await mainModel;
+	const token = await model.token.findOne({
+		where: {
+			clientId,
+			access: {
+				[Op.gte]: new Date(Date.now() - 15 * 60 * 1000)
+			}
+		}
+	});
+	if (!token) throw APIError.permissionDenied('Invalid token!');
 
-  const accessToken = token.access;
-  const foundToken = jwt.verify(
-    accessToken,
-    `${process.env.ACCESS_TOKEN_SECRET}`
-  ) as (string | jwt.JwtPayload) & AccessDecoded;
-  if (!foundToken.hasOwnProperty('UserInfo')) return null;
+	const accessToken = token.access;
+	const foundToken = jwt.verify(
+		accessToken,
+		`${process.env.ACCESS_TOKEN_SECRET}`
+	) as (string | jwt.JwtPayload) & AccessDecoded;
+	if (!Object.hasOwn(foundToken, 'UserInfo')) {
+		throw APIError.permissionDenied('Invalid credentials!');
+	}
 
-  return {
-    userID: foundToken.UserInfo.id,
-    username: foundToken.UserInfo.username,
-    role: foundToken.UserInfo.role,
-    refreshToken: token.refresh
-  };
+	return {
+		userID: foundToken.UserInfo.id,
+		username: foundToken.UserInfo.username,
+		role: foundToken.UserInfo.role,
+		refreshToken: token.refresh
+	};
 }
 
 export const auth = authHandler<AuthReq, AuthData>(mainAuth);
