1d7d8a90f01228b95a725166e43575f4b8c1474a
anekra <andika.eka.102@gmail.com>
2025-12-06 10:29:08 +0700

    Fix: add request & response types.

--- src/encore/user/routes/protected/account-route.ts
+++ src/encore/user/routes/protected/account-route.ts
@@ -1,46 +1,48 @@
 import { api } from 'encore.dev/api';
+import type { PatchAccountReq } from '../../../../types/request';
+import type { DefaultRes } from '../../../../types/response';
 import userController from '../../controllers/user-controller';
 
 export const getAccount = api(
-  {
-    method: 'GET',
-    path: '/auth-service/v1/user/account',
-    auth: true,
-    expose: true,
-    tags: ['main-model']
-  },
-  userController.getAccount
+	{
+		method: 'GET',
+		path: '/auth-service/v1/user/account',
+		auth: true,
+		expose: true,
+		tags: ['main-model']
+	},
+	userController.getAccount
 );
 
-export const patchAccount = api(
-  {
-    method: 'PATCH',
-    path: '/auth-service/v1/user/account',
-    auth: true,
-    expose: true,
-    tags: ['main-model']
-  },
-  userController.patchAccount
+export const patchAccount = api<PatchAccountReq, Promise<DefaultRes>>(
+	{
+		method: 'PATCH',
+		path: '/auth-service/v1/user/account',
+		auth: true,
+		expose: true,
+		tags: ['main-model']
+	},
+	userController.patchAccount
 );
 
 export const patchAccountEmail = api(
-  {
-    method: 'PATCH',
-    path: '/auth-service/v1/user/account/update-email',
-    auth: true,
-    expose: true,
-    tags: ['verify-request-form-otp', 'main-model']
-  },
-  userController.patchAccount
+	{
+		method: 'PATCH',
+		path: '/auth-service/v1/user/account/update-email',
+		auth: true,
+		expose: true,
+		tags: ['verify-request-form-otp', 'main-model']
+	},
+	userController.patchAccount
 );
 
 export const patchAccountUsername = api(
-  {
-    method: 'PATCH',
-    path: '/auth-service/v1/user/account/update-username',
-    auth: true,
-    expose: true,
-    tags: ['verify-request-form', 'main-model']
-  },
-  userController.patchAccount
+	{
+		method: 'PATCH',
+		path: '/auth-service/v1/user/account/update-username',
+		auth: true,
+		expose: true,
+		tags: ['verify-request-form', 'main-model']
+	},
+	userController.patchAccount
 );
