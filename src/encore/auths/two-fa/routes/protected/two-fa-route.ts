4360db0d272bee2dc60e7096f16ea248707f1aee
anekra <andika.eka.102@gmail.com>
2025-12-06 10:24:14 +0700

    Format: reformat to use type import.

--- src/encore/auths/two-fa/routes/protected/two-fa-route.ts
+++ src/encore/auths/two-fa/routes/protected/two-fa-route.ts
@@ -1,81 +1,81 @@
 import { api } from 'encore.dev/api';
-import { UsernameReq } from '../../../../../types/request';
+import type { UsernameReq } from '../../../../../types/request';
 import authAppController from '../../controllers/authapp-controller';
 import webauthnController from '../../controllers/webauthn-controller';
 
 export const generateRegisterWebauthn = api(
-  {
-    method: 'GET',
-    path: '/auth-service/v1/auth/two-fa/webauthn/register/generate',
-    auth: true,
-    expose: true,
-    tags: ['main-model', 'in-memory-model']
-  },
-  webauthnController.generateRegisterWebauthn
+	{
+		method: 'GET',
+		path: '/auth-service/v1/auth/two-fa/webauthn/register/generate',
+		auth: true,
+		expose: true,
+		tags: ['main-model', 'in-memory-model']
+	},
+	webauthnController.generateRegisterWebauthn
 );
 
 export const verifyRegisterWebauthn = api(
-  {
-    method: 'POST',
-    path: '/auth-service/v1/auth/two-fa/webauthn/register/verify',
-    auth: true,
-    expose: true,
-    tags: ['main-model', 'in-memory-model']
-  },
-  webauthnController.verifyRegisterWebauthn
+	{
+		method: 'POST',
+		path: '/auth-service/v1/auth/two-fa/webauthn/register/verify',
+		auth: true,
+		expose: true,
+		tags: ['main-model', 'in-memory-model']
+	},
+	webauthnController.verifyRegisterWebauthn
 );
 
 export const deleteWebauthnPasskey = api(
-  {
-    method: 'DELETE',
-    path: '/auth-service/v1/auth/two-fa/webauthn/passkey',
-    auth: true,
-    expose: true,
-    tags: ['main-model']
-  },
-  webauthnController.deleteWebauthnPasskey
+	{
+		method: 'DELETE',
+		path: '/auth-service/v1/auth/two-fa/webauthn/passkey',
+		auth: true,
+		expose: true,
+		tags: ['main-model']
+	},
+	webauthnController.deleteWebauthnPasskey
 );
 
 export const registerAuthApp = api(
-  {
-    method: 'GET',
-    path: '/auth-service/v1/auth/two-fa/authapp/register',
-    auth: true,
-    expose: true,
-    tags: ['main-model', 'in-memory-model']
-  },
-  authAppController.registerAuthApp
+	{
+		method: 'GET',
+		path: '/auth-service/v1/auth/two-fa/authapp/register',
+		auth: true,
+		expose: true,
+		tags: ['main-model', 'in-memory-model']
+	},
+	authAppController.registerAuthApp
 );
 
 export const verifyAuthApp = api(
-  {
-    method: 'POST',
-    path: '/auth-service/v1/auth/two-fa/authapp/verify',
-    auth: true,
-    expose: true,
-    tags: ['main-model', 'in-memory-model']
-  },
-  authAppController.verifyAuthApp
+	{
+		method: 'POST',
+		path: '/auth-service/v1/auth/two-fa/authapp/verify',
+		auth: true,
+		expose: true,
+		tags: ['main-model', 'in-memory-model']
+	},
+	authAppController.verifyAuthApp
 );
 
 export const getAuthAppToken = api<UsernameReq>(
-  {
-    method: 'GET',
-    path: '/auth-service/v1/auth/two-fa/authapp/:username',
-    auth: true,
-    expose: true,
-    tags: ['main-model']
-  },
-  authAppController.getAuthAppToken
+	{
+		method: 'GET',
+		path: '/auth-service/v1/auth/two-fa/authapp/:username',
+		auth: true,
+		expose: true,
+		tags: ['main-model']
+	},
+	authAppController.getAuthAppToken
 );
 
 export const deleteAuthAppSecret = api(
-  {
-    method: 'DELETE',
-    path: '/auth-service/v1/auth/two-fa/authapp',
-    auth: true,
-    expose: true,
-    tags: ['main-model']
-  },
-  authAppController.deleteAuthAppSecret
+	{
+		method: 'DELETE',
+		path: '/auth-service/v1/auth/two-fa/authapp',
+		auth: true,
+		expose: true,
+		tags: ['main-model']
+	},
+	authAppController.deleteAuthAppSecret
 );
