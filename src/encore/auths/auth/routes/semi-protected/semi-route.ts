336ee9d396da124953707bdabba02a24adcc9af7
anekra <andika.eka.102@gmail.com>
2025-12-06 10:23:02 +0700

    Fix: adding request & response types to refreshToken & logout endpoint. Improve: adding cache-control mw to logout endpoint.

--- src/encore/auths/auth/routes/semi-protected/semi-route.ts
+++ src/encore/auths/auth/routes/semi-protected/semi-route.ts
@@ -1,32 +1,34 @@
 import { api } from 'encore.dev/api';
+import type { RefreshTokenReq, XAuthReq } from '../../../../../types/request';
+import type { DefaultRes } from '../../../../../types/response';
 import authController from '../../controllers/auth-controller';
 
-export const refreshToken = api(
-  {
-    method: 'POST',
-    path: '/auth-service/v1/auth/refresh',
-    expose: true,
-    tags: ['main-model']
-  },
-  authController.refreshToken
+export const refreshToken = api<RefreshTokenReq, Promise<DefaultRes>>(
+	{
+		method: 'POST',
+		path: '/auth-service/v1/auth/refresh',
+		expose: true,
+		tags: ['main-model']
+	},
+	authController.refreshToken
 );
 
-export const logout = api(
-  {
-    method: 'POST',
-    path: '/auth-service/v1/auth/logout',
-    expose: true,
-    tags: ['main-model']
-  },
-  authController.logout
+export const logout = api<XAuthReq, Promise<DefaultRes>>(
+	{
+		method: 'POST',
+		path: '/auth-service/v1/auth/logout',
+		expose: true,
+		tags: ['main-model', 'logout-cache-control']
+	},
+	authController.logout
 );
 
 export const checkUsername = api(
-  {
-    method: 'GET',
-    path: '/auth-service/v1/auth/check-username',
-    expose: true,
-    tags: ['main-model']
-  },
-  authController.checkUsername
+	{
+		method: 'GET',
+		path: '/auth-service/v1/auth/check-username',
+		expose: true,
+		tags: ['main-model']
+	},
+	authController.checkUsername
 );
